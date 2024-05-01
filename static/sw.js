ce88da7ffc8f4e2a487ba09058d89d9efbae47b2
FloatSheep <sheep@hesiy.cn>
2024-05-01 21:54:39 +0800

    perf(sw.js): ⚡ 使用 MinifyJS 代替原 Service Worker的加载
    优化黑幕样式

--- static/sw.js
+++ static/sw.js
@@ -1,29 +1,91 @@
+self.importScripts("/js/localforage.min.js");
+
+let blogVersion = "";
+let checkJson = {};
+let isDataFetched = false;
+
 const NPM_REGISTRY_BASE_URL = "https://registry.npmmirror.com/";
-const packageName = "fsl-blog"; // npm包名，根据实际情况替换
+const packageName = "fsl-blog"; // 博客包名，根据实际情况替换
 const blogDomain = "blog.hesiy.cn"; // 博客域名，根据实际情况替换
-const blogVersion = "latest"; // 博客版本，根据实际情况替换
 const localMode = false; // 本地模式标志，设置为true时将忽略域名检查
+
+// 检查版本并更新
+function storageOperation(newVersion) {
+  // 定义 storageOperation 函数
+  return localforage
+    .getItem("version")
+    .then((nowVersion) => {
+      // 通过 localforage 操作 IndexedDB
+      if (!nowVersion || nowVersion !== newVersion) {
+        // 条件判断 最新版本不存在 & 当前版本不等于传入参数：newVersion
+        return localforage.setItem("version", newVersion).then(() => {
+          // 调用 localforage 更新 IndexedDB 中的值并回调
+          console.log(`版本已更新到：${newVersion}`);
+          return true; // 表示有新的版本更新
+        });
+      } else {
+        console.log("当前是最新版本");
+        return false; // 表示当前版本已是最新，无需更新
+      }
+    })
+    .catch((error) => {
+      console.error("获取或设置版本信息时出现错误:", error);
+      throw error; // 抛出错误
+    });
+}
+
+function checkUpdate(data) {
+  // 定义一个 checkUpdate 函数
+  const latestVersion = data["dist-tags"].latest; // 从传入参数：data 数组的 dist-tags 中读取 latest 值
+
+  storageOperation(latestVersion) // 调用 storageOperation 进行 IndexedDB 的更新操作
+    .then((isUpdated) => {
+      if (isUpdated) {
+        // 如果 storageOperation(data) 比对并进行了版本修改
+        console.log("博客缓存版本已更新");
+      } else {
+        // 如果 storageOperation(data) 无操作
+        console.log(`当前版本: ${latestVersion} 是最新的`);
+      }
+    })
+    .catch((error) => {
+      console.error("更新检查失败:", error);
+    });
+}
+
+function getNewestData() {
+  // 获得最新的版本信息
+  const checkURL = `${NPM_REGISTRY_BASE_URL}${packageName}`; // 拼接 API 字符串
+  self
+    .fetch(checkURL)
+    .then((response) => response.json())
+    .then((data) => checkUpdate(data)) // 将请求返回的数据：data 传给 checkUpdate 函数
+    .catch((error) => console.error("Fetch 更新信息失败:", error));
+}
+
+function fetchNewestDataIfNeeded() {
+  if (!isDataFetched) {
+    getNewestData();
+    isDataFetched = true; // 标记为已获取数据
+  }
+}
 
 self.addEventListener("install", (event) => {
   self.skipWaiting();
 });
 
 self.addEventListener("activate", (event) => {
-  event.waitUntil(self.clients.claim());
+  event.waitUntil(
+    self.clients.claim().then(() => {
+      fetchNewestDataIfNeeded();
+    })
+  );
 });
 
 self.addEventListener("fetch", (event) => {
+  fetchNewestDataIfNeeded(); // 更新版本信息
+
   const url = new URL(event.request.url);
-
-  // 检查请求的 Accept 头部是否为 EventSource 的 MIME 类型
-  const isEventSource =
-    event.request.headers.get("Accept") === "text/event-stream";
-
-  // 不拦截 EventSource 请求
-  if (isEventSource) {
-    // 如果是 EventSource 请求则不拦截，让浏览器直接处理
-    return;
-  }
 
   // 判断当前是否处于本地开发模式或者请求的域名是否为博客域名
   const shouldIntercept = localMode || url.hostname === blogDomain;
@@ -31,8 +93,8 @@ self.addEventListener("fetch", (event) => {
   // 判断请求是否需要通过NPM镜像获取且不是对Service Worker本身的请求
   const shouldHandleFetch =
     shouldIntercept &&
-    url.origin === self.location.origin &&
-    !url.pathname.includes("sw.js");
+    event.request.url.startsWith(self.location.origin) &&
+    !event.request.url.includes("sw.js");
 
   if (shouldHandleFetch) {
     let relPath = url.pathname; // 获取相对路径
@@ -42,49 +104,54 @@ self.addEventListener("fetch", (event) => {
       relPath += "index.html";
     }
 
+    // 处理版本信息
+    localforage
+      .getItem("version")
+      .then((nowVersion) => {
+        if (nowVersion) {
+          // 如果存在版本信息，则使用该版本
+          blogVersion = nowVersion;
+        } else {
+          // 如果不存在本地版本信息，则从远程获取最新数据
+          getNewestData();
+        }
+      })
+      .catch((error) => {
+        console.error("获取版本信息失败:", error);
+      });
+
     const npmPath = `${NPM_REGISTRY_BASE_URL}${packageName}/${blogVersion}/files${relPath}`;
 
     event.respondWith(
       fetch(npmPath)
         .then((response) => {
           if (!response.ok) {
-            throw new Error("NPM mirror response not ok.");
+            throw new Error("NPM Mirror 响应出现问题：范围脱离 200 ~ 299");
           }
           return corsResponse(response);
         })
         .catch((error) => {
-          console.error("Failed to fetch from NPM mirror:", error.message);
+          console.log(
+            "从 NPM Mirror 获取数据失败，退回到原请求：",
+            error.message
+          );
           return fetch(event.request); // 如果失败，则回退到原始请求
         })
     );
+  } else {
+    // 对于不符合条件的请求，保持原样不做处理
+    return fetch(event.request);
   }
-  // 不需要else，因为我们不打算使用event.respondWith()
 });
 
-self.addEventListener("message", (event) => {
-  if (event.data && event.data.action === "force-refresh") {
-    // 执行刷新逻辑
-    self.registration.update().then(() => {
-      // 清理旧的缓存
-      caches
-        .keys()
-        .then((cacheNames) => {
-          return Promise.all(cacheNames.map((cache) => caches.delete(cache)));
-        })
-        .then(() => {
-          // 更新成功后，通过 postMessage 通知客户端
-          self.clients.matchAll().then((clients) => {
-            clients.forEach((client) => client.postMessage("sw-updated"));
-          });
-        });
-    });
-  }
+self.addEventListener("terminate", (event) => {
+  isDataFetched = false; // 重置数据获取标志
 });
 
 // 处理CORS的函数
 function corsResponse(response) {
   const headers = new Headers(response.headers);
-  headers.set("Access-Control-Allow-Origin", "*"); // 在生产环境中，这个值应该设为特定的来源而非通配符 "*"
+  headers.set("Access-Control-Allow-Origin", "*");
   headers.set("Access-Control-Allow-Methods", "GET, HEAD, POST, OPTIONS");
   headers.set("Access-Control-Allow-Headers", "Content-Type");
 
