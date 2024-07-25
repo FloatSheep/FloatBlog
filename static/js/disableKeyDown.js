32a50b73dd4f5768f62c08502f1501db5b6e9860
FloatSheep <sheep@hesiy.cn>
2024-07-25 17:30:45 +0800

    fix(disableKeyDown): 🐛 修复按下「/」 时错误弹出搜索框的问题 & 添加友链

---
+++ static/js/disableKeyDown.js
@@ -0,0 +1,47 @@
+const keydownHandler = () => {};
+window.removeEventListener("keydown", keydownHandler);
+
+const changeSearchAttr = () => {
+  const searchInput = document.getElementById("search-query");
+
+  searchInput.setAttribute("autocomplete", "off");
+};
+
+const notHaveSearching = () => {
+  const searchWrapper = document.getElementById("search-wrapper");
+
+  searchWrapper.style = "visibility: hidden; display: none;";
+};
+
+window.addEventListener("DOMContentLoaded", () => {
+  changeSearchAttr();
+  document.addEventListener("keydown", function (e) {
+    if (e.key === "/") {
+      /* notHaveSearching(); */
+      hideSearch();
+      if (!searchVisible) {
+        e.preventDefault();
+      }
+    } else if (e.key === "Escape") {
+      hideSearch();
+    } else if (e.key === "ArrowDown" && searchVisible && hasResults) {
+      e.preventDefault();
+      if (document.activeElement === input) {
+        first.focus();
+      } else if (document.activeElement === last) {
+        last.focus();
+      } else {
+        document.activeElement.parentElement.nextSibling.firstElementChild.focus();
+      }
+    } else if (e.key === "ArrowUp" && searchVisible && hasResults) {
+      e.preventDefault();
+      if (document.activeElement === input) {
+        input.focus();
+      } else if (document.activeElement === first) {
+        input.focus();
+      } else {
+        document.activeElement.parentElement.previousSibling.firstElementChild.focus();
+      }
+    }
+  });
+});
