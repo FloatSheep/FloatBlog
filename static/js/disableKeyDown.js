7236811c230af2f06280e7f669f6da0051847162
FloatSheep <admin@hesiy.cn>
2025-06-27 21:07:47 +0800

    fix(disableKeyDown): 🐛 修复无法输入 / 的问题

--- static/js/disableKeyDown.js
+++ static/js/disableKeyDown.js
@@ -1,40 +1,10 @@
-const keydownHandler = () => {};
-window.removeEventListener("keydown", keydownHandler);
-
-const changeSearchAttr = () => {
-  const searchInput = document.getElementById("search-query");
-
-  searchInput.setAttribute("autocomplete", "off");
-};
-
-window.addEventListener("DOMContentLoaded", () => {
-  document.addEventListener("keydown", function (e) {
-    if (e.key === "/") {
-      hideSearch();
-      if (!searchVisible) {
-        e.preventDefault();
-      }
-    } else if (e.key === "Escape") {
-      hideSearch();
-    } else if (e.key === "ArrowDown" && searchVisible && hasResults) {
-      e.preventDefault();
-      if (document.activeElement === input) {
-        first.focus();
-      } else if (document.activeElement === last) {
-        last.focus();
-      } else {
-        document.activeElement.parentElement.nextSibling.firstElementChild.focus();
-      }
-    } else if (e.key === "ArrowUp" && searchVisible && hasResults) {
-      e.preventDefault();
-      if (document.activeElement === input) {
-        input.focus();
-      } else if (document.activeElement === first) {
-        input.focus();
-      } else {
-        document.activeElement.parentElement.previousSibling.firstElementChild.focus();
-      }
-    }
-  });
-  changeSearchAttr();
+/** 修复 / */
+document.addEventListener("keydown", function (e) {
+  const focusElement = document.activeElement;
+  if (focusElement.id === "wl-edit" && e.key === "/") {
+    searchVisible = true;
+  } else if (focusElement.id === "search-query" && e.key === "/") {
+  } else if (focusElement.id !== "wl-edit" && e.key === "/" && searchVisible) {
+    searchVisible = false;
+  }
 });
